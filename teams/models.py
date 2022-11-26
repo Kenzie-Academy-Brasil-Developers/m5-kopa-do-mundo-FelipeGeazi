5222ffe984a3e2aef57c30a0b4fddac129a9938d
FelipeGeazi <felipegeaziartes@gmail.com>
2022-11-26 00:06:42 -0300

    1ª parte

---
+++ teams/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Team (models.Model):
+    name = models.CharField(max_length=30)
+    titles = models.IntegerField(default=0 )
+    top_scorer = models.CharField(max_length=50)
+    fifa_code = models.CharField(max_length=3)
+    founded_at = models.DateField
+
+    def __repr__(self):
+        # self.pk (primary key)
+        return f"<{self.id} {self.name} - {self.fifa_code}>"
